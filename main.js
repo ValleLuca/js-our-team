ec6d81d641fbc49d6a3b20648f1124dbd4660426
ValleLuca <valle.lucaAb@outlook.it>
2022-01-10 18:28:07 +0100

    Fine esercizio con aggiunta di commenti

--- main.js
+++ main.js
@@ -31,9 +31,9 @@ const team = [
   },
 ];
 
-const container = document.querySelector(".team-container");
+const container = document.querySelector(".team-container");                             //creata variabile per container
 
-for(let i = 0; i < team.length; i++){
+for(let i = 0; i < team.length; i++){                                                   //crato ciclo per card base 
   container.innerHTML += `
   <div class="team-card">
     <div class="card-image" id="pushImgCarta">
@@ -47,11 +47,26 @@ for(let i = 0; i < team.length; i++){
 `;
 }
 
-document.getElementById("addMemberButton").addEventListener("click", function() {
-  const takeName = document.getElementById("name");
-  const takeRole = document.getElementById("role");
-  const takeImage = document.getElementById("image");
-  console.log(takeName);
-  console.log(takeRole);
-  console.log(takeImage);
+
+document.getElementById("addMemberButton").addEventListener("click", function() {     //funziona che si abilita al click sul Form all'interno del nostro dom
+  const takeName = document.getElementById("name").value;                             //ottenuti dato "nome"
+  const takeRole = document.getElementById("role").value;                             //ottenuti dato "ruolo"
+  const takeImage = document.getElementById("image").value;                           //ottenuti dato "immagine descrittiva"
+
+  const datiUtenteForm = {name: takeName, role: takeRole, image: takeImage,};         //Creato nuovo oggetto con i dati recepiti dal form immessi dalll'utente
+  team.push(datiUtenteForm);                                                          //Aggiunta dell'oggetto all'interno dell'array
+
+  container.innerHTML +=                                                            //aggiunta della nuova carta attraverso i dati reccepiti dal form
+  `                                                            
+  <div class="team-card">
+    <div class="card-image" id="pushImgCarta">
+      <img src="${datiUtenteForm.image}" id="provaImg">
+    </div>
+    <div class="card-text">
+      <h3 id="pushNomeCarta">${datiUtenteForm.name}</h3>
+      <p id="pushRuoloCarta">${datiUtenteForm.role}</p>
+    </div>
+  </div>
+`;
+
 });
