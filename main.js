4abcda2cb8950667835316d32b58cb5e4ff80d88
ValleLuca <valle.lucaAb@outlook.it>
2022-01-10 16:04:02 +0100

    Modificata la tabella per rispondere ai comandi js

--- main.js
+++ main.js
@@ -2,34 +2,44 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
+
+const nomeCarta = team[0].name;
+pushNomeCarta.innerHTML = (nomeCarta);
+
+const ruoloCarta = team[1].role;
+pushRuoloCarta.innerHTML = (ruoloCarta);
+
+const imgCarta = team[2].image;
+const prova = document.getElementById("provaImg").src = (imgCarta);
+
 
 //ottenere gli input per creare nuovi profili
 //una volta ottenuti i dati dal Dom prendere i dati e inserirli in un nuovo oggetto
